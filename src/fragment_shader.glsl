d396124cd3f2aaea39cf2f7a7dbac2a15a7a0223
Zim <98758788+hazimbharis@users.noreply.github.com>
2025-03-25 14:06:26 +0800

    added a triangle
    - added readme on how to quickly build on my system

---
+++ src/fragment_shader.glsl
@@ -0,0 +1,7 @@
+#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(0.196f, 0.62f, 0.478f, 1.0f);
+} 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <glad/glad.h>
 
 #include <glm/common.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
@@ -10,7 +13,175 @@
 #include <backends/imgui_impl_opengl3.h>
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
+void onStart()
+{
+  // Example usage
+  //int vertexShader = LoadShader("vertex_shader.glsl", GL_VERTEX_SHADER);
+  //int fragmentShader = LoadShader("fragment_shader.glsl", GL_FRAGMENT_SHADER);
+}
+
+int LoadShader(const std::string shaderFilePath, GLenum shaderType)
+{
+  std::ifstream shaderFile(shaderFilePath);
+  if (!shaderFile.is_open()) {
+      std::cerr << "Failed to open shader file: " << shaderFilePath << std::endl;
+      return -1;
+  }
+
+  std::stringstream shaderStream;
+  shaderStream << shaderFile.rdbuf(); // Read file contents
+  std::string shaderCode = shaderStream.str();
+  const char* shaderSource = shaderCode.c_str();
+
+  // Create and compile the shader
+  unsigned int shaderID = glCreateShader(shaderType);
+  glShaderSource(shaderID, 1, &shaderSource, nullptr);
+  glCompileShader(shaderID);
+
+  // Check for compilation errors
+  int success;
+  char infoLog[512];
+  glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
+  if (!success) {
+      glGetShaderInfoLog(shaderID, 512, nullptr, infoLog);
+      std::cerr << "Shader compilation error (" << shaderFilePath << "):\n" << infoLog << std::endl;
+      return -1;
+  }
+
+  return shaderID;
+}
+
+void GLAPIENTRY DebugMessageCallback(
+  GLenum source,
+  GLenum type,
+  GLuint id,
+  GLenum severity,
+  [[maybe_unused]] GLsizei length,
+  const GLchar* message,
+  [[maybe_unused]] const void* userParam)
+{
+  // Ignore certain verbose info messages (particularly ones on Nvidia).
+  if (id == 131169 || 
+      id == 131185 || // NV: Buffer will use video memory
+      id == 131218 || 
+      id == 131204 || // Texture cannot be used for texture mapping
+      id == 131222 ||
+      id == 131154 || // NV: pixel transfer is synchronized with 3D rendering
+      id == 0         // gl{Push, Pop}DebugGroup
+  )
+  return;
+
+  std::stringstream debugMessageStream;
+  debugMessageStream << message << '\n';
+
+  switch (source)
+  {
+      case GL_DEBUG_SOURCE_API: debugMessageStream << "Source: API"; break;
+      case GL_DEBUG_SOURCE_WINDOW_SYSTEM: debugMessageStream << "Source: Window Manager"; break;
+      case GL_DEBUG_SOURCE_SHADER_COMPILER: debugMessageStream << "Source: Shader Compiler"; break;
+      case GL_DEBUG_SOURCE_THIRD_PARTY: debugMessageStream << "Source: Third Party"; break;
+      case GL_DEBUG_SOURCE_APPLICATION: debugMessageStream << "Source: Application"; break;
+      case GL_DEBUG_SOURCE_OTHER: debugMessageStream << "Source: Other"; break;
+  }
+
+  debugMessageStream << '\n';
+
+  switch (type)
+  {
+      case GL_DEBUG_TYPE_ERROR: debugMessageStream << "Type: Error"; break;
+      case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: debugMessageStream << "Type: Deprecated Behaviour"; break;
+      case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: debugMessageStream << "Type: Undefined Behaviour"; break;
+      case GL_DEBUG_TYPE_PORTABILITY: debugMessageStream << "Type: Portability"; break;
+      case GL_DEBUG_TYPE_PERFORMANCE: debugMessageStream << "Type: Performance"; break;
+      case GL_DEBUG_TYPE_MARKER: debugMessageStream << "Type: Marker"; break;
+      case GL_DEBUG_TYPE_PUSH_GROUP: debugMessageStream << "Type: Push Group"; break;
+      case GL_DEBUG_TYPE_POP_GROUP: debugMessageStream << "Type: Pop Group"; break;
+      case GL_DEBUG_TYPE_OTHER: debugMessageStream << "Type: Other"; break;
+  }
+
+  debugMessageStream << '\n';
+
+  switch (severity)
+  {
+      case GL_DEBUG_SEVERITY_HIGH: debugMessageStream << "Severity: high"; break;
+      case GL_DEBUG_SEVERITY_MEDIUM: debugMessageStream << "Severity: medium"; break;
+      case GL_DEBUG_SEVERITY_LOW: debugMessageStream << "Severity: low"; break;
+      case GL_DEBUG_SEVERITY_NOTIFICATION: debugMessageStream << "Severity: notification"; break;
+  }
+
+    std::cout << "OpenGL Message:" << type << debugMessageStream.str() << std::endl;
+}
+
+void renderTriangle()
+{
+  // put in onstart()
+  float vertices[] = {
+    -0.5f, -0.75f, 0.0f, // bottom left vertex
+     0.5f, -0.75f, 0.0f, // bottom right vertex
+     0.5f,  0.75f, 0.0f, // top right vertex
+     -0.5f,  0.75f, 0.0f // top left vertex
+  };
+
+  // put in onstart()
+  unsigned int indices[] = {  // note that we start from 0!
+    0, 1, 3,   // first triangle
+    1, 2, 3    // second triangle
+  };  
+
+  // ..:: Initialization code (done once (unless your object frequently changes)) :: ..
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+  // 1. bind Vertex Array Object
+  glBindVertexArray(VAO);
+
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  // 0. copy our vertices array in a buffer for OpenGL to use
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);  
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // we use element buffer objects to reuse vertex to draw triangles with overlapping points
+  unsigned int EBO;
+  glGenBuffers(1, &EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);  
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  int vertexShader = LoadShader("C:/Users/azrom/Downloads/opengl-template-main/src/vertex_shader1.glsl", GL_VERTEX_SHADER);
+  int fragmentShader = LoadShader("C:/Users/azrom/Downloads/opengl-template-main/src/fragment_shader.glsl", GL_FRAGMENT_SHADER);
+
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);  
+
+    // 1. then set the vertex attributes pointers
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);  
+
+  // 2. use our shader program when we want to render an object
+  glUseProgram(shaderProgram);
+
+  glm::mat4 vec = glm::mat4(1.0f);
+  //vec = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+ // vec = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
+  //vec = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));  
+  unsigned int transformLoc = glGetUniformLocation(vertexShader, "transform");
+  glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(vec));
+
+  // 3. bind the VAO we're going to use to switch between different vertex arrays easy
+  glBindVertexArray(VAO);
+  // 4. now draw the object 
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  // 5. unbind vao
+  glBindVertexArray(0);
+}
 
 int main()
 {
@@ -49,6 +220,17 @@ int main()
   ImGui_ImplOpenGL3_Init("#version 150");
   ImGui::StyleColorsDark();
 
+  glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+  glm::mat4 trans = glm::mat4(1.0f);
+  trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+  trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
+  trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));  
+  vec = trans * vec;
+  //std::cout << vec.x << vec.y << vec.z << std::endl;
+
+  glDebugMessageCallback(DebugMessageCallback, nullptr);
+  glEnable(GL_DEBUG_OUTPUT);
+  glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
   
   while (!glfwWindowShouldClose(window))
   {
@@ -63,6 +245,7 @@ int main()
       
     glClearColor(red, 0.0, 0.0, 1.0);
 
+    renderTriangle();
 
     ImGui_ImplGlfw_NewFrame();
     ImGui_ImplOpenGL3_NewFrame();
