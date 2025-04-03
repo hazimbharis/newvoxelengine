4f508f112b48e13d8cfec11c8c7525ba215277b4
Zim <98758788+hazimbharis@users.noreply.github.com>
2025-04-03 13:00:01 +0800

    added rotating 3d cubes

--- src/main.cpp
+++ src/main.cpp
@@ -122,7 +122,7 @@ void GLAPIENTRY DebugMessageCallback(
     std::cout << "OpenGL Message:" << type << debugMessageStream.str() << std::endl;
 }
 
-void renderTriangle(float rot, unsigned int shaderProgram, unsigned int VAO, unsigned int texture)
+void renderTriangle(float rot, unsigned int shaderProgram, unsigned int VAO, unsigned int texture, glm::vec3 cubePositions[])
 {
   // 2. use our shader program when we want to render an object
   glUseProgram(shaderProgram);
@@ -130,17 +130,47 @@ void renderTriangle(float rot, unsigned int shaderProgram, unsigned int VAO, uns
   glm::mat4 vec = glm::mat4(1.0f);
   //vec = glm::translate(vec, glm::vec3(1.0f, 1.0f, 0.0f));
   //vec = glm::rotate(vec, glm::radians(rot), glm::vec3(0.0f, 0.0f, 1.0f));
-  vec = glm::rotate(vec, (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
+  //vec = glm::rotate(vec, (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
   //vec = glm::scale(vec, glm::vec3(0.5, 0.5, 0.5));  
   unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
   glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(vec));
+
+  // the model matrix to render our first 3D object
+  glm::mat4 model = glm::mat4(1.0f);
+  //model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+  model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));  
+  int modelLoc = glGetUniformLocation(shaderProgram, "model");
+  glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+  glm::mat4 view = glm::mat4(1.0f);
+  // note that we're translating the scene in the reverse direction of where we want to move
+  view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); 
+  int viewLoc = glGetUniformLocation(shaderProgram, "view");
+  glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+  glm::mat4 projection;
+  projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 1000.0f);
+  int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+  glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
   // 2.5 bind texture before drawing
   glBindTexture(GL_TEXTURE_2D, texture);
   // 3. bind the VAO we're going to use to switch between different vertex arrays easy
   glBindVertexArray(VAO);
   // 4. now draw the object 
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  for(unsigned int i = 0; i < 10; i++)
+  {
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, cubePositions[i]);
+    float angle = 20.0f * i; 
+    model = glm::rotate(model, glm::radians(angle) + (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(1.0f, 0.3f, 0.5f));
+    int modelLoc = glGetUniformLocation(shaderProgram, "model");
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+  }
+  //glDrawArrays(GL_TRIANGLES, 0, 36);
   // 5. unbind vao
   glBindVertexArray(0);
 }
@@ -191,17 +221,75 @@ int main()
   // std::cout << vec.x << vec.y << vec.z << std::endl;
 
   // Initialize all the variables
+  // float vertices[] = {
+  //   -0.5f, -0.75f, 0.0f, 0.0f, 0.0f,  // lower-left corner  
+  //    0.5f, -0.75f, 0.0f, 1.0f, 0.0f,  // lower-right corner   
+  //    0.5f,  0.75f, 0.0f, 1.0f, 1.0f,  // top-right corner 
+  //    -0.5f,  0.75f, 0.0f, 0.0f, 1.0f  // top-left corner 
+  // };
+
+  // the vertices of one cube - O_O
   float vertices[] = {
-    -0.5f, -0.75f, 0.0f, 0.0f, 0.0f,  // lower-left corner  
-     0.5f, -0.75f, 0.0f, 1.0f, 0.0f,  // lower-right corner   
-     0.5f,  0.75f, 0.0f, 1.0f, 1.0f,  // top-right corner 
-     -0.5f,  0.75f, 0.0f, 0.0f, 1.0f  // top-left corner 
-  };
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
 
   unsigned int indices[] = {  // note that we start from 0!
     0, 1, 3,   // first triangle
     1, 2, 3    // second triangle
   };  
+
+  glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f), 
+    glm::vec3( 2.0f,  5.0f, -15.0f), 
+    glm::vec3(-1.5f, -2.2f, -2.5f),  
+    glm::vec3(-3.8f, -2.0f, -12.3f),  
+    glm::vec3( 2.4f, -0.4f, -3.5f),  
+    glm::vec3(-1.7f,  3.0f, -7.5f),  
+    glm::vec3( 1.3f, -2.0f, -2.5f),  
+    glm::vec3( 1.5f,  2.0f, -2.5f), 
+    glm::vec3( 1.5f,  0.2f, -1.5f), 
+    glm::vec3(-1.3f,  1.0f, -1.5f)  
+};
 
   // Run the initialize buffer functions
   unsigned int VAO;
@@ -253,7 +341,7 @@ int main()
   // Load texture
   int width, height, nrChannels;
   stbi_set_flip_vertically_on_load(true);  
-  unsigned char *data = stbi_load("C:\\Users\\azrom\\Documents\\GitHub\\newvoxelengine\\textures\\GfLpMT4asAYLY8J.png", &width, &height, &nrChannels, 0);
+  unsigned char *data = stbi_load("C:\\Users\\azrom\\Documents\\GitHub\\newvoxelengine\\textures\\GimdAa9bYAIVQBa.jpg", &width, &height, &nrChannels, 0);
   if (data) {
       glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
       glGenerateMipmap(GL_TEXTURE_2D);
@@ -262,15 +350,15 @@ int main()
   }
   stbi_image_free(data);
 
-
   glDebugMessageCallback(DebugMessageCallback, nullptr);
   glEnable(GL_DEBUG_OUTPUT);
   glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+  glEnable(GL_DEPTH_TEST);  
   
   float rotation = 0.0f;
   while (!glfwWindowShouldClose(window))
   {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 
     red = delta;
@@ -281,7 +369,7 @@ int main()
       
     glClearColor(red, 0.0, 0.0, 1.0);
 
-    renderTriangle(rotation, shaderProgram, VAO, texture);
+    renderTriangle(rotation, shaderProgram, VAO, texture, cubePositions);
     rotation += 0.01f;
 
 
